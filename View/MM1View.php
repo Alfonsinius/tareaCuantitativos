2570d4c44c559e0a0df935e0f459fd67b3db5a96
Jose Alfonso Jiménez Ulloa <alfonsoaju6@gmail.com>
2020-06-28 02:26:35 -0600

    update

--- View/MM1View.php
+++ View/MM1View.php
@@ -9,11 +9,11 @@ include 'Menu.html';
     <h2><span class="label label-default">Formulario para resolver ejercicios tipo MM1</span></h2>
     <form action = "../Data/MM1.php" method="post">
         <h3><span class="label label-default">Digite el valor de lambda (λ)</span></h3>
-        <input name="lambda" type="number" class="form-control" placeholder="número de links">
+        <input name="lambda" type="number" class="form-control" placeholder="valor de λ">
 
 
         <h3><span class="label label-default">Digite el valor de mi (μ)</span></h3>
-        <input name="mi" type="number" class="form-control" placeholder="número de links">
+        <input name="mi" type="number" class="form-control" placeholder="valor de μ">
         <center><input type="submit" name="BtnGuardar" value="Calcular" class="btn btn-primary btn-lg"></center> 
 
     </form>
