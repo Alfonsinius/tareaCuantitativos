ebb192c8906f1242d40b0aceb0dbfbebbc16567e
Jose Alfonso Jiménez Ulloa <alfonsoaju6@gmail.com>
2020-06-28 00:29:30 -0600

    les hice una vista a cada uno
    Es decir tienen un archivo para que pongan la logica y otro para que pongan la vista

---
+++ View/MM1View.php
@@ -0,0 +1,14 @@
+<?php ?>
+
+<?php
+include 'Menu.html';
+?>
+
+<div class="container">
+<h3><span class="label label-default">Digite el valor de lambda (λ)</span></h3>
+<input name="links" type="number" class="form-control" placeholder="número de links"  step="0.01">
+
+
+<h3><span class="label label-default">Digite el valor de mi (μ)</span></h3>
+<input name="links" type="number" class="form-control" placeholder="número de links"  step="0.01">
+</div>
---
+++ View/MMnView.php
@@ -0,0 +1,4 @@
+<?php
+
+include './Menu.html';
+echo 'Aqui va la vista para resolver ejercicios de tipo MMn Infinito';
