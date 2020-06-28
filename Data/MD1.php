8743edacae6f6027cafdc3c6e3c3ba842624e6f9
Mariela Calvo <aleiram2811@gmail.com>
2020-06-28 15:01:27 -0600

    Calculador MD1
    Calculador MD1

--- Data/MD1.php
+++ Data/MD1.php
@@ -1,4 +1,24 @@
 <?php
   include '../View/MenuLogica.html';
-echo 'Aqui va el codigo para resolver ejercicios de tipo MD1';
 
+  $lambda = $_POST['lambda'];
+  $miu    = $_POST['mi'];
+  $num    = 1;
+
+  $div = (2*$miu*($miu-$lambda));
+  $Lq  = pow($lambda,2)/$div;
+  $Wq  = $lambda / $div;
+  $L   = $Lq + ($lambda/$miu);
+  $W   = $Wq + ($num/$miu);
+?>
+
+<div class="container">
+  <h1>El valor de Lq  es:</h1>
+  <p>{{$Lq}}</p>
+  <h1>El valor de Wq  es:</h1>
+  <p>{{$Wq}}</p>
+  <h1>El valor de L   es:</h1>
+  <p>{{$L}}</p>
+  <h1>El valor de Q   es:</h1>
+  <p>{{$W}}</p>
+</div>
--- View/MD1View.php
+++ View/MD1View.php
@@ -1,4 +1,22 @@
 <?php
 
 include 'Menu.html';
-echo 'Aqui va la vista para resolver ejercicios de tipo MD1';
+
+?>
+
+<div class="container">
+    <form action="../Data/MD1.php" method="post" class="form-inline">
+        <div class="form-group">
+            <label for="inputPromedio">Digite el valor de &micro;:</label>
+            <input class="form-control" type="number" id="inputPromedio">  
+        </div>
+        <div class="form-group">
+            <label for="inputLamba">Digite el valor &lambda;:</label>
+            <input class="form-control" type="number" id="inputLambda">
+        </div>
+        <button class="btn btn-success">Calcular</button>
+    </form>
+    
+    
+    
+</div>
