7e55ce0da0daf1d697f3805604fceccff37ad52c
Dilan Angulo Ruiz <dilan0400ar@gmail.com>
2020-06-29 15:58:13 -0600

    MM1 Infinito

--- Data/MM1Infinito.php
+++ Data/MM1Infinito.php
@@ -1,6 +1,50 @@
-<?php
-    include '../View/MenuLogica.html';
+﻿<?php
+include '../View/MenuLogica.html';
+                                                                                       
+//valor
+$valorU= $_POST["valorU"];
+$valorH=$_POST['valorH'];
+$valorN=$_POST['valorN'];
 
-echo 'Aqui va el codigo para resolver ejercicios de tipo MM1 INFINITO';
+ //valor calcular
+ $L=0.0;
+ $W=0.0;
+ $Lq=0.0;
+ $Wq=0.0;
+ $Po=0.0;
+ 
+ $Po=(1/obtieneDenominadorPo($valorN, $valorH, $valorU));
+ $Lq=($valorN-(($valorH+$valorU)/$valorH)*(1-$Po));
+ $L=($Lq+(1-$Po));
+ $W=$L/$valorH;
+ $Wq=($Lq/(($valorN-$L)*$valorH));
+ 
+ $MinutosW=$W*60;
+ $MinutosWq=$Wq*60;
+ 
+ function obtieneFactorial($numero){ 
+    $factorial = 1; 
+    for ($i = 1; $i <= $numero; $i++){ 
+      $factorial = $factorial * $i; 
+    } 
+    return $factorial; 
+} 
 
+function obtieneDenominadorPo($limite, $valorH, $valorU){
+    $denominadorPo = 0;
+    for($i = 0; $i <= $limite; $i++) {
+        $denominadorPo = $denominadorPo + 
+                (obtieneFactorial($i)/obtieneFactorial($limite-$i))*
+                (($valorH/$valorU)^$i);
+    }
+    return $denominadorPo;
+}
+?>
 
+<h1><span class="label label-danger">El resultado de MM1 infinito para un valor de la tasa de llegadas en promedio de: <?php echo $valorH ?>,  valor de la tasa de servicios en promedio de: <?php echo $valorU?> ,  numero de canales abiertos de: <?php echo $valorM?> y N de: <?php echo $valorN?>   es el siguiente:</span></h1><br><br>
+
+<center><h1> <span class="label label-success"><?php echo'El numero promedio de clientes (unidades) dentro del sistema es de: ' . $L; ?> </span></h1></center><hr>
+<center><h1> <span class="label label-success"><?php echo'El tiempo promedio en el sistema es de: ' . $W. ' convertido en minutos sería: '.$MinutosW; ?> </span></h1></center><hr>
+<center><h1> <span class="label label-success"><?php echo'La longitud promedio de la cola es: ' . $Lq; ?> </span></h1></center><hr>
+<center><h1> <span class="label label-success"><?php echo'El tiempo de espera promedio en la cola es: ' . $Wq. ' convertido en minutos sería: '.$MinutosWq; ?> </span></h1></center><hr>
+<center><h1> <span class="label label-success"><?php echo'La probabilidad de que el sistema este vacio es de ' . ($Po); ?> </span></h1></center><hr>
--- View/MM1InfinitoView.php
+++ View/MM1InfinitoView.php
@@ -1,4 +1,29 @@
-<?php
+<?php include './Menu.html'; ?>
 
-include './Menu.html';
-echo 'Aqui va la vista para resolver ejercicios de tipo MM1 Infinito';
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="../Data/MM1Infinito.php" method="post">
+            <label for="fname">Ingrese el valor de la tasa de llegadas promedio:</label><br>
+            <input type="number"  name="valorU"><br>
+            <label for="lname">Ingrese el valor de la tasa de llegadas en promedio:</label><br>
+            <input type="number" name="valorH"><br><br>
+            <label for="lname">Ingrese el valor de N:</label><br>
+            <input type="number"  name="valorN"><br><br>
+            
+            <input type="submit" value="Calcular">
+        </form>
+<?php
+// put your code here
+?>
+    </body>
+</html>
