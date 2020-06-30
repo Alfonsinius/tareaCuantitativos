652d6c30969a49e85141b0629761da3676a39963
Josseline Matamoros <matamoros1997@hotmail.com>
2020-06-29 22:25:55 -0600

    actualización

--- View/MMnView.php
+++ View/MMnView.php
@@ -16,13 +16,11 @@ and open the template in the editor.
         <form action="../Data/MMN.php" method="post">
             <label for="fname">Ingrese el valor de la tasa de llegadas promedio:</label><br>
             <input type="number"  name="valorU"><br>
-            <label for="lname">Ingrese el valor de la tasa de llegadas en promedio:</label><br>
+            <label for="lname">Ingrese el valor de la tasa de servicio en promedio:</label><br>
             <input type="number" name="valorH"><br><br>
-            <label for="lname">Ingrese el valor de numero de canales abiertos :</label><br>
+            <label for="lname">Ingrese el valor de M:</label><br>
             <input type="number" name="valorM"><br><br>
-            <label for="lname">Ingrese el valor de N:</label><br>
-            <input type="number"  name="valorN"><br><br>
-            <label for="lname">Costos:</label><br>
+                        <label for="lname">Costos:</label><br>
             <label for="lname">Horas laboradas:</label><br>
             <input type="number"  name="valorHoras"><br><br>
 
